0d1006fdb4ed12699c923d16d83e047c483dfd7a
aolgac <aolgac@gmail.com>
2017-02-25 22:52:11 +0200

    scenario 2
    scenario 2 moves the files matching the criteria to the directory

--- move.py
+++ move.py
@@ -1,6 +1,15 @@
+#Scenario 1
+import os, shutil
+shutil.move("85_FLAP_desmond_setup","85")
+#Scenario 2
+import glob
+for myfile in myfiles:
+  first_part = myfile[:-19]
+  dir = glob.glob('%s/' % first_part)[0]
+  os.rename(myfile, os.path.join(dir, myfile))
+#Scenario 3 - PS: Not finished
 import os, shutil, fnmatch
 files=[]
 for file in os.listdir("."):
   if fnmatch.fnmatch(file, "*_FLAP_desmond_setup"):
     files.append(file)
-  shutil.move("85_FLAP_desmond_setup","85")
