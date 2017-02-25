eea536b65b2e9a906be3f624f827965845dab3e3
aolgac <aolgac@gmail.com>
2017-02-25 22:09:03 +0200

    will continue later

--- move.py
+++ move.py
@@ -1,2 +1,6 @@
-import os, shutil
-shutil.move("85_FLAP_desmond_setup","85")
+import os, shutil, fnmatch
+files=[]
+for file in os.listdir("."):
+  if fnmatch.fnmatch(file, "*_FLAP_desmond_setup"):
+    files.append(file)
+  shutil.move("85_FLAP_desmond_setup","85")
